fca840e1b8e497a10e801a898adcd55f6c88ea23
Kaysier De los Reyes <69897182+kyzier23@users.noreply.github.com>
2020-09-01 16:53:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php
 print "<html>\n";
 print "<link rel=\"stylesheet\" href=\"css/style.css\">\n";
 print "<body>\n";
@@ -6,3 +7,4 @@ print "<div class=\"d\"></div>\n";
 print "<div class=\"a\"></div>	\n";
 print "</body>\n";
 print "</html>";
+?>
