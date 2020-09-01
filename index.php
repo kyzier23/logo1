eacbf3b205639abf812f386992f3d589092b9dd1
Kaysier De los Reyes <69897182+kyzier23@users.noreply.github.com>
2020-09-01 16:50:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?php include_once(“home.html”); ?>
+print "<html>\n";
+print "<link rel=\"stylesheet\" href=\"css/style.css\">\n";
+print "<body>\n";
+print "<div class=\"k\"></div>\n";
+print "<div class=\"d\"></div>\n";
+print "<div class=\"a\"></div>	\n";
+print "</body>\n";
+print "</html>";
